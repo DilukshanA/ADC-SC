99ceb18c70b23c3068f8c616452b57b798c1b4c1
Dilukshan <dilukshan2020da@gmail.com>
2025-03-10 18:34:43 +0530

    Update main.c

--- ADC-SC/main.c
+++ ADC-SC/main.c
@@ -3,12 +3,23 @@
 #include <util/delay.h>
 
 void initHardware(void){
-	DDRA = 0B11111111; // setting all A pins to output
-	DDRC = 0B11111111; //setting all C pins to output
+	DDRA |= 0B11111111; //0xFF // setting all A pins to output
+	DDRC |= 0B11111111; //setting all C pins to output
 }
 
 void initADC(void){
+	//AREF, External
+	ADMUX &= ~((1<<REFS1)|(1<<REFS0));
 	
+	//enabling ADC
+	ADCSRA |= (1<<ADEN);
+	
+	//setting the prescaler to 128 to get optimal clock frequency for ADC
+	ADCSRA |= (1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2);
+	
+	//selecting the AC0
+	//ADMUX &= ~((1<<MUX0)|(1<<MUX1)|(1<<MUX2)|(1<<MUX3)|(1<<MUX4));
+	//ADCSRB &= ~(1<<MUX5);
 }
 
 
