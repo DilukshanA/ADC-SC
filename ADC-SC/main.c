218717e56b1275730c27f3149b7e107bc4178c8a
Dilukshan <dilukshan2020da@gmail.com>
2025-03-10 10:28:26 +0530

    Update main.c

--- ADC-SC/main.c
+++ ADC-SC/main.c
@@ -1,12 +1,11 @@
-/*
- * ADC-SC.c
- *
- * Created: 3/10/2025 8:44:55 AM
- * Author : ASUS
- */ 
-
+#define F_CPU 16000000UL
 #include <avr/io.h>
+#include <util/delay.h>
 
+void initHardware(void){
+	DDRA = 0B11111111; // setting all A pins to output
+	DDRC = 0B11111111; //setting all C pins to output
+}
 
 int main(void)
 {
